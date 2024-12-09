93790f996eb1f4f8276be38348360fd7f8db0093
Parsa <deadlockfree504@gmail.com>
2024-12-10 02:40:17 +0330

    added interactive state , needs to be updated later

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,17 +1,28 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializer import BlogSerializer
-from base.models import BlogModel  
+from base.models import BlogModel, PublishState, Category  
 from rest_framework import status
-
 
 
 
 
 @api_view(['GET'])
 def viewData(request):
-    Blog = BlogModel.objects.all()
-    serializer = BlogSerializer(Blog,many=True) 
+    state = request.GET.get('state')
+    if state:
+        try:
+            state = int(state)
+            if state in [PublishState.State.DRAFT, PublishState.State.PUBLISHED, PublishState.State.ARCHIVED]:
+                blogs = BlogModel.objects.filter(state__state=state)
+            else:
+                return Response({'error': 'Invalid state.'}, status=400)
+        except ValueError:
+            return Response({'error': 'State must be an integer.'}, status=400)
+    else:
+        blogs = BlogModel.objects.all()
+    
+    serializer = BlogSerializer(blogs, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
