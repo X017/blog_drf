c072b6886f42421918bf5a94b7465e262c006b2c
Parsa <deadlockfree504@gmail.com>
2024-12-10 12:06:40 +0330

    added one-way basic authtencation

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views 
-from .views import Blog_View
+from .views import Blog_View , User_View
 
 urlpatterns = [
-    path('blogs/',Blog_View.as_view(),name='blog-list')
+    path('blogs/',Blog_View.as_view(),name='blog-list'),
+    path('comments/',User_View.as_view(),name='blog-comments')
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from base.models import User, Blog
-from .serializer import BlogSerializer
+from .serializer import BlogSerializer, CommentSerializer
 
 class Blog_View(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs,many=True)
@@ -19,5 +22,12 @@ class Blog_View(generics.ListAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
 
-class Comment_View(generics):
-    pass
+
+class User_View(generics.GenericAPIView):
+    queryset = User
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        comments = User.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
