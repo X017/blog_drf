c8059c4cdb36fcb1b184893fa0ed9cbc805d8dd5
Parsa <deadlockfree504@gmail.com>
2024-12-10 11:25:01 +0330

    updated Blog_View class

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
+from rest_framework.response import Response
+
 from base.models import User, Blog
 from .serializer import BlogSerializer
 
-class Blog_View(generics.ListCreateAPIView):
+class Blog_View(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs,many=True)
+        return Response(serializer.data)
 
