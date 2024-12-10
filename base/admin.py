b0d81dc841f9aafc2e0bfbb74577b5aa516fb789
Parsa <deadlockfree504@gmail.com>
2024-12-10 11:09:06 +0330

    renamed the models name , changed the views.py decorators to class based

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import BlogModel, PublishState, Category, UserComment
+from .models import Blog, Category, User, PublishState
 # Register your models here.
 
-admin.site.register(BlogModel)
 admin.site.register(PublishState)
+admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(UserComment)
+admin.site.register(Blog)
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
+from .views import Blog_View
 
 urlpatterns = [
-    path('',views.viewData),
-    path('add/',views.postData),
+    path('blogs/',Blog_View.as_view(),name='blog-list')
 ]
